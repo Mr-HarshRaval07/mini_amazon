863debafb8047f42498122e1e0461ba0ea4c930f
Mr-HarshRaval07 <harshsamirraval@gmail.com>
2026-02-10 13:43:03 +0530

    Initial commit

--- products/signals.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Product
-from search.services import trie
-
-@receiver(post_save, sender=Product)
-def update_trie(sender, instance, created, **kwargs):
-    trie.insert(instance.name)
